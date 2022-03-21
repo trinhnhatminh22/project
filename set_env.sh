7cf729468c3cc1fe9a78c10fc5b5d2adf3e221c2
trinhnhatminh22 <59037890+trinhnhatminh22@users.noreply.github.com>
2022-03-21 15:26:33 +0700

    Initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=ls4user1807676
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=ls4user1807676.clkgiw4wcx4u.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=ls4-user1807676-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
